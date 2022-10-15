2200e8b4d6f7b04a1d5cc028fb5f10141c1f54f4
Juan Jose Sierra <jota853@hotmail.com>
2022-10-15 09:16:11 -0500

    auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $req){
+
+        $validate = $req->validate([
+            'name'=>'required|string|max:255',
+            'email'=>'required|string|email|max:255|unique:users',
+            'password'=>'required|string|min:8'
+
+        ]);
+
+        $user = User::create([
+            'name'=>$validate['name'],
+            'email'=>$validate['email'],
+            'password'=>Hash::make($validate['password'])
+
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token'=>$token,
+            'token_type'=>'Bearer'
+
+        ]);
+
+    }
+
+
+    public function login(Request $req){
+        
+        if(!Auth::attempt($req->only('email','password'))){
+            return response()->json([
+                'mensaje'=>'invalido',
+            ], 401);
+        }
+
+        $user = User::where('email',$req->email)->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token'=>$token,
+            'token_type'=>'Bearer'
+        ]);
+    }
+
+
+    public function infouser(Request $req){
+        return $req->user();
+    }
+
+    public function logout(){
+        Auth::user()->tokens()->delete();
+        return response()->json([
+                'mensaje'=>'cerrado session',
+        ], 200);
+
+    }
+}
