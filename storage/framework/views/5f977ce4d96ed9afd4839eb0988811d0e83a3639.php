33626238787cf171d8f1b4153aad3417af0872f7
Juan Jose Sierra <juan.sierra@cajamag.com.co>
2022-10-19 08:15:02 -0500

    cambio de rama a main

---
+++ storage/framework/views/5f977ce4d96ed9afd4839eb0988811d0e83a3639.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>Pokegrid - Jsierra</title>
+    <!-- Additional CSS Files -->
+    <?php echo app('Illuminate\Foundation\Vite')('resources/statics/bootstrap/css/bootstrap.min.css'); ?>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/statics/css/fontawesome.css'); ?>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/statics/css/templatemo-cyborg-gaming.css'); ?>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/statics/css/owl.css'); ?>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/statics/css/animate.css'); ?>
+
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+
+
+    <?php echo app('Illuminate\Foundation\Vite')('resources/css/app.css'); ?>
+  </head>
+
+<body>
+  <div id="app"></div>  
+  
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <?php echo app('Illuminate\Foundation\Vite')('resources/statics/jquery/jquery.min.js'); ?>
+  <?php echo app('Illuminate\Foundation\Vite')('resources/statics/bootstrap/js/bootstrap.min.js'); ?>
+  <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
+
+  </body>
+
+</html>
+<?php /**PATH /var/www/html/netgrid/resources/views/inicio.blade.php ENDPATH**/ ?>
